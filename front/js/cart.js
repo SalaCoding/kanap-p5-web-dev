6b6cf8f4c3d00c10b17a3e79b4891175ec162725
Iyoke Sala <salaarnold14@gmail.com>
2022-09-28 14:52:10 +0100

    cart

--- front/js/cart.js
+++ front/js/cart.js
@@ -2,127 +2,43 @@
  const retrivStoredFile = JSON.parse(localStorage.getItem('store'));
  console.log(retrivStoredFile)
  
-//CART IMAGE
-const cartImg = document.getElementsByClassName('cart__item__img');
-let newImage = document.createElement('img');
-cartImg.innerHTML = `<img src="${retrivStoredFile.imgUrl}" alt="${retrivStoredFile.altTxt}">`;
-console.log(cartImg)
+ 
+ //GE ACCES TO THE DOM
+ const cart__items = document.getElementById('cart__items');
+ const cart__item = document.createElement('article');
+ const cart__item__img = document.createElement('div');
+ const cart__item__content = document.createElement('div');
+ const cart__item__content__description = document.createElement('div');
+ const h2 = document.createElement('h2');
+ const p__color = document.createElement('p');
+ const p__price = document.createElement('p');
+ const cart__item__content__settings = document.createElement('div');
+ const cart__item__content__settings__quantity = document.createElement('div');
+ const qte = document.createElement('p');
+ const input = document.createElement('input');
+ const cart__item__content__settings__quantity__delete = document.createElement('div');
+ const p__deleteItem = document.createElement('p');
+ 
+ for (i = 0 ; i < retrivStoredFile.length ; i++) {
+    //cart__item.setAttribute(`data-id="${retrivStoredFile.productId}" data-color="${retrivStoredFile.color}" `);
+    //cart__item__img.setAttribute('src', retrivStoredFile.imgUrl);
+    //cart__item__img.innerHTML = `<img src="${retrivStoredFile.imgUrl}" alt="${retrivStoredFile.altTxt}">`;
+    h2.textContent += retrivStoredFile.title[i];
+    p__color.textContent = retrivStoredFile.color[i];
+    
+ }
+ cart__items.appendChild(cart__item);
+    cart__item.appendChild(cart__item__img);
+    cart__item.appendChild(cart__item__content);
+    cart__item__content.appendChild(cart__item__content__description);
+    cart__item__content__description.appendChild(h2);
+    cart__item__content__description.appendChild(p__color);
+    cart__item__content__description.appendChild(p__price);
+    cart__item__content.appendChild(cart__item__content__settings);
+    cart__item__content__settings.appendChild(cart__item__content__settings__quantity);
+    cart__item__content__settings__quantity.appendChild(qte);
+    cart__item__content__settings__quantity.appendChild(input);
+    cart__item__content__settings.appendChild(cart__item__content__settings__quantity__delete);
+    cart__item__content__settings__quantity__delete.appendChild(p__deleteItem);
 
-//Cart content
-const cartContent = document.getElementById('cart__item__content');
-//Description of the product
-const cartItemDescription = document.getElementsByClassName('cart__item__content__description');
-const childCartItem = document.getElementsByTagName('h2').textContent = retrivStoredFile.title;
-const secondChildCartItem = childCartItem[0].textContent = retrivStoredFile.color;
-const thirdChildCartItem = childCartItem[1].textContent = retrivStoredFile.price;
-// Set the number of the items
-const inputQuantity = document.querySelector('.itemQuantity');
-//    const deleteBtn = document.getElementById('deleteItem');
-console.log(childCartItem)
-console.log(secondChildCartItem)
-console.log(thirdChildCartItem)
-
-
-
-
-
-
-
-
-
-
-//Calculation
-const newObject = {
-    min: 1,
-    max: 100,
-}
-for (let i = 0; i < newObject.max; i++){
-    //totalOfPrice += inputQuantity * thirdChildCartItem;
-}
-
-const resetForm = document.querySelector('.cart__order__form');
-
-
-//Quantity of the product
-const totalItem = document.getElementById('totalQuantity');
-totalItem.textContent = retrivStoredFile.quantity;
-const totalOfPrice = document.getElementById('totalPrice');
-
-//    const costumerName = document.getElementById('firstName');
-//    const costumerLastName = document.getElementById('lastName');
-//    const errorMsg = document.getElementById('lastNameErrorMsg');
-//    errorMsg.textContent = 'Veillez entrer votre nom correct!';
-
-//    const costumerAddress = document.getElementById('address');
-//    const addressErrorMsg = document.getElementById('addressErrorMsg');
-//    addressErrorMsg.textContent = 'Veillez entrer votre une addresse correcte!';
-
-//    const costumerCity = document.getElementById('city');
-//    const cityErrorMsg = document.getElementById('cityErrorMsg');
-//    cityErrorMsg.textContent = 'Veillez entrer votre une ville correcte!';
-
-//    const costumeremail = document.getElementById('email');
-//    const emailErrorMsg = document.getElementById('emailErrorMsg');
-//    emailErrorMsg.textContent = 'Veillez entrer votre un email correct!';
-
-//    const btnOrder = document.getElementById('order');
-
-//Send informations
-
-//inputQuantity.addEventListener('change', ($event) => {
-//    totalItem.textContent = $event.target.value;
-//});
-
-
-/**THIS IS FOR THE FORM
- * THE BUTTON IS ON DEFAULT
- * IT'S GETTING VALUES
- * AND WHEN YOU CLICK THE BUTTON THE FIELDS ARE RESET
- */ 
-//btnOrder.addEventListener('click', ($event) => {
-//    $event.preventDefault();
-//    const post = {
-//        name: costumerName.value,
-//        lastName: costumerLastName.value,
-//        address: costumerAddress.value,
-//        city: costumerCity.value,
-//        email: costumeremail.value
-//    }
-//    resetForm.reset();
-//});
-
-
-/**
- * Make a request
- */
-//function makeRequest(data) {
-//    return new Promise((resolve, reject) => {
-//        let request = new XMLHttpRequest();
-//        request.open('POST', api + '/create-post');
-//        request.onreadystatechange = () => {
-//            if (request.readyState == 4) {
-//                if (request.status == 201) {
-//                    resolve(JSON.parse(request.response))
-//                }else {
-//                    reject(JSON.parse(request.response));
-//                }
-//            }
-//        };
-//        request.setRequestHeader('content-Type', 'application/json');
-//        request.send(JSON.stringify(data))
-//    })
-//}
-//}
-
-//async function submitForm(post) {
-//    try {
-//        const requestPromise = makeRequest(post);
-//        const response = await requestPromise;
-//        costumerName.textContent = response.firstName;
-//        costumerLastName.textContent = response.lastName;
-//    } catch (err) {
-//        responseMessage.textContent = err.error
-//    }
-//}
-//submitForm();
-
+    cart__items.appendChild(cart__item);
