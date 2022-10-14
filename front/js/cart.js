8670d1f4614e18722d0024bf449cd365341ccc11
Iyoke Sala <salaarnold14@gmail.com>
2022-10-14 02:03:56 +0100

    loop for the cart

--- front/js/cart.js
+++ front/js/cart.js
@@ -32,9 +32,10 @@ localStorage.setItem('cart', JSON.stringify(cart));
    cart__item.setAttribute('data', products.id);
    cart__item.setAttribute('data-color', products.color);
   console.log(cart__item)
-
+  
+  
    cart__item__imgUrl.setAttribute('src', products.imgUrl);
-    cart__item__imgUrl.setAttribute('alt', products.altText);
+   cart__item__imgUrl.setAttribute('alt', products.altText);
    
     //NAME, COLOR, PRICE AND QUANTITY
     h2.innerText = products.title;
@@ -81,7 +82,35 @@ localStorage.setItem('cart', JSON.stringify(cart));
 
     cart__items.appendChild(cart__item);
 
-   
+    content = "";
+    for (let i = 0; i < cart.length; i++) {
+      const item = cart[i];
+      content += `
+      <article class="cart__item" data-id="${item.id}" data-color="${item.color}">
+      <div class="cart__item__img">
+      <img src="${item.imgUrl}" alt="${item.altText}">
+    </div>
+    <div class="cart__item__content">
+      <div class="cart__item__content__description">
+        <h2>${item.title}</h2>
+        <p>${item.color}</p>
+        <p>${item.price}</p>
+      </div>
+      <div class="cart__item__content__settings">
+        <div class="cart__item__content__settings__quantity">
+          <p>Qté : </p>
+          <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${item.quantity}">
+        </div>
+        <div class="cart__item__content__settings__delete">
+          <p class="deleteItem">Delete</p>
+        </div>
+      </div>
+    </div>
+    </article>
+         `;
+    }
+    cart__items.innerHTML = content;
+
    //GET ACCESS TO THE DOM  AND VALUE 
    const firstName = document.getElementById('firstName').value;
    const lastName = document.getElementById('lastName').value;
